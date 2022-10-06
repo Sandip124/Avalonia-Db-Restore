03f1313a853c758ff4a7011934e9777e44352a3d
Sandip Chaudhary <chaudharysandip124@gmail.com>
2022-10-06 14:53:22 +0545

    refactor: command executer

--- PostgresRestore/Helper/Commandexecuter.cs
+++ PostgresRestore/Helper/Commandexecuter.cs
@@ -5,70 +5,64 @@ using PostgresRestore.Vo;
 
 namespace PostgresRestore.Helper;
 
-  public static class CommandExecutor
+public static class CommandExecutor
+{
+    public static void Execute(string commandType, string user, string database)
     {
-        public static void Execute(string commandType, string user, string database)
-        {
-            var proc = new Process();
-            proc.StartInfo.FileName = "psql";
-            proc.StartInfo.Arguments = $@"-U {user} -c ""{commandType} database """"{database}""""";
-            proc.StartInfo.RedirectStandardOutput = true;
-            proc.StartInfo.RedirectStandardError = true;
-            proc.StartInfo.UseShellExecute = false;
-            proc.StartInfo.CreateNoWindow = true;
-            proc.Start();
-            var output = proc.StandardOutput.ReadToEnd();
-            var error = proc.StandardError.ReadToEnd();
-            proc.WaitForExit();
-            if (proc.ExitCode != 0)
-            {
-                proc.Close();
-                throw new Exception("Error restoring database: " + error);
-            }
-
-            proc.Close();
-        }
-
-        public static void ExecuteRestore(UserConnectionVo connection)
-        {
-            Environment.SetEnvironmentVariable(PostgresConstants.PasswordKey, connection.Password);
-            switch (connection.ActionTypeValue)
-            {
-                case ActionTypeConstants.DropAndRestore:
-                    Execute("drop", connection.UserName, connection.DatabaseName);
-                    Execute("create", connection.UserName, connection.DatabaseName);
-                    break;
-                case ActionTypeConstants.CreateAndRestore:
-                    Execute("create", connection.UserName, connection.DatabaseName);
-                    break;
-            }
-
-            var proc = new Process();
-            if (connection.DatabaseBackupType == CommandTypeConstants.PgDump)
-            {
-                proc.StartInfo.FileName = "psql";
-                proc.StartInfo.Arguments = $@"-U {connection.UserName} ""{connection.DatabaseName}"" < ""{connection.RestoreFileLocation}""";
-            }
-            else
-            {
-                proc.StartInfo.FileName = connection.DatabaseBackupType;
-                proc.StartInfo.Arguments = $@"-U {connection.UserName} -d ""{connection.DatabaseName}"" ""{connection.RestoreFileLocation}""";
-            }
-
-            proc.StartInfo.RedirectStandardOutput = true;
-            proc.StartInfo.RedirectStandardError = true;
-            proc.StartInfo.UseShellExecute = false;
-            proc.StartInfo.CreateNoWindow = true;
-            proc.Start();
-            var output = proc.StandardOutput.ReadToEnd();
-            var error = proc.StandardError.ReadToEnd();
-            proc.WaitForExit();
-            if (proc.ExitCode != 0)
-            {
-                proc.Close(); 
-                throw new Exception("Error restoring database.Details: " + error);
-            }
-
-            proc.Close();
-        }
+        ExecuteCommand("psql", $@"-U {user} -c ""{commandType} database """"{database}""""");
     }
+
+    public static void ExecuteRestore(UserConnectionVo connection)
+    {
+        Environment.SetEnvironmentVariable(PostgresConstants.PasswordKey, connection.Password);
+        switch (connection.ActionTypeValue)
+        {
+            case ActionTypeConstants.DropAndRestore:
+                Execute("drop", connection.UserName, connection.DatabaseName);
+                Execute("create", connection.UserName, connection.DatabaseName);
+                break;
+            case ActionTypeConstants.CreateAndRestore:
+                Execute("create", connection.UserName, connection.DatabaseName);
+                break;
+        }
+
+        string arguments;
+        string fileName;
+        if (connection.DatabaseBackupType == CommandTypeConstants.PgDump)
+        {
+            fileName = "psql";
+            arguments =
+                $@"-U {connection.UserName} ""{connection.DatabaseName}"" < ""{connection.RestoreFileLocation}""";
+        }
+        else
+        {
+            fileName = connection.DatabaseBackupType;
+            arguments =
+                $@"-U {connection.UserName} -d ""{connection.DatabaseName}"" ""{connection.RestoreFileLocation}""";
+        }
+
+        ExecuteCommand(fileName, arguments);
+    }
+
+
+    private static void ExecuteCommand(string fileName, string arguments)
+    {
+        var proc = new Process();
+        proc.StartInfo.FileName = fileName;
+        proc.StartInfo.Arguments = arguments;
+        proc.StartInfo.RedirectStandardOutput = true;
+        proc.StartInfo.RedirectStandardError = true;
+        proc.StartInfo.UseShellExecute = false;
+        proc.StartInfo.CreateNoWindow = true;
+        proc.Start();
+        var error = proc.StandardError.ReadToEnd();
+        proc.WaitForExit();
+        if (proc.ExitCode != 0)
+        {
+            proc.Close();
+            throw new Exception("Error restoring database: " + error);
+        }
+
+        proc.Close();
+    }
+}
